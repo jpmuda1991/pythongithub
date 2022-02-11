58a7e507da732ed8c3a28ddd3bf5d0676edb86b1
Mudaheranwa <jpmuda@outlook.fr>
2022-02-11 05:57:48 -0500

    first commit

---
+++ video10 loops/10loops.py
@@ -0,0 +1,14 @@
+#Lien video youtube:
+
+
+#une boucle qui imprime tout les nombres paires entre  2 et 28
+
+i =2
+e  =2
+while (i <= 28):
+    print("nombre croissant: ", i )
+    i *= 2    #incrementation par la multiplication
+
+while (e <=28):
+    print("nombre croissant" , e)
+    e += 2      #incrementation par l'addition
---
+++ video10 loops/10loopspart2.py
@@ -0,0 +1,14 @@
+#suite: boucle "for"
+
+#Liste de noms d"automobile
+
+
+autos = ["ford" ,"toyota" , "hyundai" , "Honda" , "Acura", "Mitsubishi"
+         ,"Ford","KYA"]
+
+
+i =1
+
+for x in autos:
+    print(i,"-", x)
+    i +=1
